caee0859e91ee5fca59826c1a7e21da9e33fb8ec
apple <875172749@qq.com>
2018-07-17 03:04:10 +0800

    newsPage

--- client/pages/newsPage/newsPage.js
+++ client/pages/newsPage/newsPage.js
@@ -1,33 +1,41 @@
 Page({
   data:{
-    text:"Page newsPage",
+    posterTexts:["一柒一会，我们收获了什么","创投干货：对话投资人"],
+    posterImage:["./poster1.jpeg","./poster2.jpg"],
     newsList:[{
       id:"sj-000001",
-      date:"201807151647",
-      from:"南大射箭社",
-      title:"南大射箭社招新啦",
-      subTitle:"来加入我们的大家庭吧",
+      title:"国际关系学社到底发生了什么，昨夜的狂欢到底为何？",
+      from:"国际关系学社",
+      like:"12",
+      date:"3小时前",
       cover:""
     },{
       id:"sj-000001",
-      date:"201807151647",
-      from:"南大射箭社",
-      title:"南大射箭社招新啦",
-      subTitle:"来加入我们的大家庭吧",
+      date:"12小时前",
+      from:"反哺学社",
+      title:"反哺学社新学期招募计划，平安夜义卖了解一下~",
+      like:"24",
       cover:""
     },{
       id:"sj-000001",
-      date:"201807151647",
+      date:"昨天",
       from:"南大射箭社",
       title:"南大射箭社招新啦",
-      subTitle:"来加入我们的大家庭吧",
+      like:"90",
       cover:""
     },{
       id:"sj-000001",
-      date:"201807151647",
-      from:"南大射箭社",
-      title:"南大射箭社招新啦",
-      subTitle:"来加入我们的大家庭吧",
+      date:"昨天",
+      from:"跆拳道社",
+      title:"致最爱跆拳道的你",
+      like:"88",
+      cover:""
+    },{
+      id:"sj-000001",
+      date:"3天前",
+      from:"南大团委",
+      title:"全国大学生龙舟大赛拉开序幕",
+      like:"268",
       cover:""
     }]
   },
